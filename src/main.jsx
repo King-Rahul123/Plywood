f93fb2ca15c83a178574550b905c3319bd6b2b08
King-Rahul123 <adakrahul15@gmail.com>
2026-01-17 20:15:43 +0530

    v

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "./App";
 import "./index.css";
 
